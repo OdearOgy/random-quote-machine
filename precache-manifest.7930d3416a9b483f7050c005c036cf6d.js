f8e3bdc0eaee2224269838265c482e8d96aa6910
OdearOgy <hovhan198@gmail.com>
2019-11-15 13:35:18 +0400

    Updates

---
+++ precache-manifest.7930d3416a9b483f7050c005c036cf6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ea4e891e253520984d99b7a58dd7cc",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "5c86da812ae0a1b57cfd",
+    "url": "/random-quote-machine/static/css/main.cec6a7de.chunk.css"
+  },
+  {
+    "revision": "a4103e920dffa07d5e9a",
+    "url": "/random-quote-machine/static/js/2.3b54e953.chunk.js"
+  },
+  {
+    "revision": "5c86da812ae0a1b57cfd",
+    "url": "/random-quote-machine/static/js/main.2ddc10d4.chunk.js"
+  },
+  {
+    "revision": "8857fb9802ceefb5660d",
+    "url": "/random-quote-machine/static/js/runtime-main.62b08fbf.js"
+  }
+]);
